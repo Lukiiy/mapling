f3de8deb6dbf38c8c16d0b4b795777f9aac89eef
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-06-22 20:07:34 -0300

    Add serialized Position identifier "pos:"

--- src/main/kotlin/me/lukiiy/mapling/Position.kt
+++ src/main/kotlin/me/lukiiy/mapling/Position.kt
@@ -1,13 +1,14 @@
 package me.lukiiy.mapling
 
 data class Position(val x: Double, val y: Double, val z: Double, val yaw: Float = 0f, val pitch: Float = 0f) {
-    fun serialize(): String = "$x;$y;$z;$yaw;$pitch"
+    fun serialize(): String = "pos:$x;$y;$z;$yaw;$pitch"
 
     companion object {
         fun deserialize(value: String): Position {
-            val parts = value.split(';')
+            require(value.startsWith("pos:")) { "Invalid position." }
 
-            require(parts.size == 5) { "Invalid position." }
+            val parts = value.substring(4).split(';')
+            if (parts.size != 5) throw IllegalArgumentException("Invalid position.")
 
             return Position(parts[0].toDouble(), parts[1].toDouble(), parts[2].toDouble(), parts[3].toFloat(), parts[4].toFloat())
         }
