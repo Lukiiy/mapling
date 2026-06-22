7e2e926dc0f9f0397db1e3cc669089ffc2abda90
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-06-22 14:30:46 -0300

    Remove Serializable and use require

--- src/main/kotlin/me/lukiiy/mapling/Position.kt
+++ src/main/kotlin/me/lukiiy/mapling/Position.kt
@@ -1,14 +1,13 @@
 package me.lukiiy.mapling
 
-import java.io.Serializable
-
-data class Position(val x: Double, val y: Double, val z: Double, val yaw: Float = 0f, val pitch: Float = 0f) : Serializable {
+data class Position(val x: Double, val y: Double, val z: Double, val yaw: Float = 0f, val pitch: Float = 0f) {
     fun serialize(): String = "$x;$y;$z;$yaw;$pitch"
 
     companion object {
         fun deserialize(value: String): Position {
             val parts = value.split(';')
-            if (parts.size != 5) throw IllegalArgumentException("Invalid position.")
+
+            require(parts.size == 5) { "Invalid position." }
 
             return Position(parts[0].toDouble(), parts[1].toDouble(), parts[2].toDouble(), parts[3].toFloat(), parts[4].toFloat())
         }
