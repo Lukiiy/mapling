973062585b1d8bd03a4c551726170e7c8f1a8792
Lukiiy <lukiiy@proton.me>
2026-06-22 13:57:40 -0300

    refactor location into position

--- src/main/kotlin/me/lukiiy/mapling/Location.kt
+++
@@ -1,5 +0,0 @@
-package me.lukiiy.mapling
-
-import java.io.Serializable
-
-data class Location(val x: Double, val y: Double, val z: Double, val yaw: Float = 0f, val pitch: Float = 0f, ) : Serializable
