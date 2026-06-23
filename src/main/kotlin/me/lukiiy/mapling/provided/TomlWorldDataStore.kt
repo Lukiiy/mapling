0f16e04b7e4068ebe0df726525cdd378efc32387
Lukiiy <lukiiy@proton.me>
2026-06-22 21:02:40 -0300

    Add TomlWorldDataStore

---
+++ src/main/kotlin/me/lukiiy/mapling/provided/TomlWorldDataStore.kt
@@ -0,0 +1,12 @@
+package me.lukiiy.mapling.provided
+
+import dev.eav.tomlkt.Toml
+import kotlinx.serialization.encodeToString
+import me.lukiiy.mapling.WorldData
+import me.lukiiy.mapling.WorldDataStore
+import java.io.File
+
+class TomlWorldDataStore : WorldDataStore {
+    override fun load(file: File): WorldData = WorldData.fromToml(Toml.parseToTomlTable(file.readText()))
+    override fun save(file: File, data: WorldData) = file.writeText(Toml.encodeToString(data.toToml()))
+}
