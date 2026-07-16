aea57e6741b76572f02acde68639c2c2f3eaeb7b
Lukiiy <lukiiy@proton.me>
2026-07-16 17:26:41 -0300

    Add javadocs to WorldDataStore

--- src/main/kotlin/me/lukiiy/mapling/WorldDataStore.kt
+++ src/main/kotlin/me/lukiiy/mapling/WorldDataStore.kt
@@ -2,11 +2,25 @@ package me.lukiiy.mapling
 
 import java.io.File
 
+/**
+ * An interface to create all sorts of files to save/load a [WorldData]!
+ */
 interface WorldDataStore {
     companion object {
         const val INCOMPATIBLE = "Unsupported value."
     }
 
+    /**
+     * Decode a [WorldData] from a given file
+     * @param file The file
+     * @return A data holder
+     */
     fun load(file: File): WorldData
+
+    /**
+     * Encode a given [WorldData] into a file
+     * @param file The file that will be used
+     * @param data The data
+     */
     fun save(file: File, data: WorldData)
 }
